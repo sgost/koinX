f61ceff0fa1d3ad9f61a02d04baab46eccbc8c23
Manoj Ponugoti <jameslucky917@gmail.com>
2022-12-12 12:37:27 +0530

    UPDATE: changes done in index.js file updated blogs initiall links

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -5,7 +5,7 @@ const BlogPage = ({ data }) => {
 
   const posts = useStaticQuery(graphql`
     query {
-      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(mine)\\/.*\\\\.md$/"}}, sort: { fields: [frontmatter___date], order: DESC }, limit: 3) {
+      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(blog)\\/.*\\\\.md$/"}}, sort: { fields: [frontmatter___date], order: DESC }, limit: 3) {
         edges {
           node {
             id
@@ -40,14 +40,15 @@ const BlogPage = ({ data }) => {
     <div style={{ background: '#000' }}>
       <div className="blogListContainer">
         <h1 style={{ color: `red` }}>How To Buy ?</h1>
-        <Link to={blogData[0].node.fields.slug} id="blog_card">
-          <h2 style={{ color: `blue` }}>{blogData[0].node.frontmatter.navLinkTitle}</h2>
-        </Link>
-
+        {blogData?.map((item) =>
+          <Link to={item.node.fields.slug} id="blog_card">
+            <h2 style={{ color: `blue` }}>{item.node.frontmatter.navLinkTitle}</h2>
+          </Link>)}
+        {/* 
         <h1 style={{ color: `red`, marginTop: `10px` }}>How to mine ?</h1>
         <Link to={blogData[0].node.fields.slug} id="blog_card">
           <h2 style={{ color: `blue` }}>{blogData[0].node.frontmatter.navLinkTitle}</h2>
-        </Link>
+        </Link> */}
       </div>
     </div>
   )
